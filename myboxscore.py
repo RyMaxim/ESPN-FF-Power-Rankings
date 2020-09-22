be99b9a43bebfca273c36ba8731d216c68053988
RyMaxim <59573579+RyMaxim@users.noreply.github.com>
2020-09-22 12:52:05 -0400

    fix IR player support

--- myboxscore.py
+++ myboxscore.py
@@ -31,42 +31,42 @@ class MyBoxScore():
         for player in self.lineup:
             self.bench_points += player.points if player.slot_position == 'BE' else 0
 
-    def get_top_player(self):  # top scoring non-bench player
+    def get_top_player(self):  # top scoring non-bench, non-IR player
         self.lineup = sorted(self.lineup, key=lambda x: x.points, reverse=True)
         for player in self.lineup:
-            if player.slot_position != 'BE':
+            if player.slot_position != 'BE' and player.slot_position != 'IR':
                 self.top_player = player
                 break
 
-    def get_bottom_player(self):  # top scoring non-bench player
+    def get_bottom_player(self):  # top scoring non-bench, non-IR player
         self.lineup = sorted(self.lineup, key=lambda x: x.points, reverse=False)
         for player in self.lineup:
-            if player.slot_position != 'BE':
+            if player.slot_position != 'BE' and player.slot_position != 'IR':
                 self.bottom_player = player
                 break
 
-    def get_overachiever(self):  # highest delta between points and projection for non-bench players
+    def get_overachiever(self):  # highest delta between points and projection for non-bench, non-IR players
         for player in self.lineup:
             player.diff = round(player.points - player.projected_points,2)
         self.lineup = sorted(self.lineup, key=lambda x: x.diff, reverse=True)
         for player in self.lineup:
-            if player.slot_position != 'BE':
+            if player.slot_position != 'BE' and player.slot_position != 'IR':
                 self.overachiever = player
                 break
 
-    def get_underachiever(self):  # lowest delta between points and projection for non-bench players
+    def get_underachiever(self):  # lowest delta between points and projection for non-bench, non-IR players
         for player in self.lineup:
             player.diff = round(player.points - player.projected_points,2)
         self.lineup = sorted(self.lineup, key=lambda x: x.diff, reverse=False)
         for player in self.lineup:
-            if player.slot_position != 'BE':
+            if player.slot_position != 'BE' and player.slot_position != 'IR':
                 self.underachiever = player
                 break
 
-    def get_goose_eggs(self):  # non-bench players who scored 0 or less points
+    def get_goose_eggs(self):  # non-bench, non-IR players who scored 0 or less points
         self.goose_eggs = list()
         for player in self.lineup:
-            if player.slot_position != 'BE' and player.points <= 0:
+            if player.slot_position != 'BE' and player.slot_position != 'IR' and player.points <= 0:
                 self.goose_eggs.append(player)
 
     def fix_flex_players(self):  # convert position text for FLEX players to 'FLX'
